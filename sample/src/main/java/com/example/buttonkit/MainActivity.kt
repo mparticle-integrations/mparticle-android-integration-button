f09e13520ff79488217b3bf1797b45a0a18437fe
Najm Sheikh <najm@usebutton.com>
2020-08-19 17:24:38 -0400

    Update demo app

--- sample/src/main/java/com/example/buttonkit/MainActivity.kt
+++ sample/src/main/java/com/example/buttonkit/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = MParticle.getInstance().getKitInstance(
+        val button = MParticle.getInstance()?.getKitInstance(
                 MParticle.ServiceProviders.BUTTON) as ButtonKit?
 
         bt_attribution_refresh.setOnClickListener {
